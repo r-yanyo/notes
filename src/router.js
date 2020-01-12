241374386d8259e13f0b5f59cbd479fd19bdd5ea
yanyo <honma.ryosuke@gmail.com>
2020-01-12 17:13:48 +0900

    [Delete] unnecessary page

--- src/router.js
+++ src/router.js
@@ -1,7 +1,6 @@
 import Vue from "vue";
 import Router from "vue-router";
 import Home from "./views/Home.vue";
-import About from "./views/About.vue";
 import Login from "./views/Login.vue";
 import auth from "./utils/auth";
 
@@ -14,12 +13,6 @@ const router = new Router({
       path: "/",
       name: "home",
       component: Home,
-      meta: { requiresAuth: true }
-    },
-    {
-      path: "/about",
-      name: "about",
-      component: About,
       meta: { requiresAuth: true }
     },
     {
